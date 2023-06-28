cbb6c88db15c285034f7c6069bf711dd359a9894
Thomas Honeyman <hello@thomashoneyman.com>
2023-06-27 22:09:01 -0400

    Update to latest purix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    purescript-nix.url = "github:thomashoneyman/purescript-nix";
-    purescript-nix.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    purix.url = "github:thomashoneyman/purix";
+    purix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs: let
@@ -11,20 +11,18 @@
     utils.eachSupportedSystem = inputs.utils.lib.eachSystem utils.supportedSystems;
 
     mkPackages = pkgs: let
-      npmDependencies = pkgs.buildPackageLock {src = ./.;};
-      workspaces = pkgs.buildSpagoLock {src = ./.;};
+      npmDependencies = pkgs.purix.buildPackageLock {src = ./.;};
+      spagoDependencies = pkgs.purix.buildSpagoLock {src = ./.;};
     in {
       # Build the PureScript package and bundle to a Node script.
       default = pkgs.stdenv.mkDerivation {
         name = "my-app";
         src = ./my-app;
         phases = ["buildPhase" "installPhase"];
-        nativeBuildInputs = [pkgs.purs.purs-unstable pkgs.esbuild];
+        nativeBuildInputs = [pkgs.esbuild];
         buildPhase = ''
           ln -s ${npmDependencies}/js/node_modules .
-          set -f
-          purs compile $src/**/*.purs ${workspaces.my-app.dependencies.globs}
-          set +f
+          cp -r ${spagoDependencies.my-app}/output .
           esbuild ./output/App.Main/index.js --bundle --outfile=app.js --platform=node --minify
         '';
         installPhase = ''
@@ -47,14 +45,14 @@
 
     mkDevShells = pkgs: {
       default = pkgs.mkShell {
-        buildInputs = [pkgs.purs.purs-unstable pkgs.spago.spago pkgs.esbuild];
+        buildInputs = [pkgs.purs-unstable pkgs.spago-unstable pkgs.esbuild];
       };
     };
 
     mkOutput = system: let
       pkgs = import inputs.nixpkgs {
         inherit system;
-        overlays = [inputs.purescript-nix.overlays.default];
+        overlays = [inputs.purix.overlays.default];
       };
     in rec {
       packages = mkPackages pkgs;
